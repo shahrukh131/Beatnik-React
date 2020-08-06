33becad59dc3dedc57d3d685b94d0556c3ee9534
Shahrukh-akib <Shahrukhakib@gmail.com>
2020-08-06 11:53:11 +0600

    Updates

---
+++ precache-manifest.f3c6084509afc01f5c27629d1649e836.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b47e86baf7dfa07eaca899676866fab3",
+    "url": "/Beatnik-React/index.html"
+  },
+  {
+    "revision": "33d3ff9776c13b93bb94",
+    "url": "/Beatnik-React/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "0488b12f6c139a7e91ba",
+    "url": "/Beatnik-React/static/css/main.87660760.chunk.css"
+  },
+  {
+    "revision": "33d3ff9776c13b93bb94",
+    "url": "/Beatnik-React/static/js/2.138f0bdc.chunk.js"
+  },
+  {
+    "revision": "bfb3d1277f3c8df032112d2c58ceb601",
+    "url": "/Beatnik-React/static/js/2.138f0bdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0488b12f6c139a7e91ba",
+    "url": "/Beatnik-React/static/js/main.cb0954c4.chunk.js"
+  },
+  {
+    "revision": "7a7f0f9582c27a418842",
+    "url": "/Beatnik-React/static/js/runtime-main.94e7bcbc.js"
+  },
+  {
+    "revision": "c2b5532c001ec43174609a9fd0b63bb0",
+    "url": "/Beatnik-React/static/media/1.c2b5532c.jpg"
+  },
+  {
+    "revision": "101b2b430016bc9badc44376195f7b77",
+    "url": "/Beatnik-React/static/media/2.101b2b43.jpg"
+  },
+  {
+    "revision": "b51af0d31281a515cc05924a450381a5",
+    "url": "/Beatnik-React/static/media/iamge2.b51af0d3.jpg"
+  },
+  {
+    "revision": "35f0cbe2479f5b66252d77e5ccc3b53c",
+    "url": "/Beatnik-React/static/media/world5.35f0cbe2.png"
+  }
+]);
